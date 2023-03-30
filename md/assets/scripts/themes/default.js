c2c19af4cca01247c4eae99d87b05f08f3ad8408
九凌少子 <64707090+asir3q@users.noreply.github.com>
2023-03-31 01:21:52 +0800

    Create default.js

---
+++ md/assets/scripts/themes/default.js
@@ -0,0 +1,124 @@
+var defaultTheme = {
+  BASE: {
+    'text-align': 'left',
+    'color': '#333',
+    'line-height': '2'
+  },
+  BASE_BLOCK: {
+    
+  },
+  block: {
+    h2: {
+      'margin': '0 0 30px',
+      'padding': '0 0 0 12px',
+      'font-weight': '700',
+      'line-height': '1.4',
+      'font-size': '24px',
+      'border-left': '4px solid #f1404b'
+    },
+    h3: {
+      'margin': '0 0 30px',
+      'padding': '0 0 0 12px',
+      'font-weight': '700',
+      'line-height': '1.4',
+      'font-size': '22px',
+      'border-left': '4px solid #f1404b'
+    },
+    p: {
+      'margin': '0 0 30px',
+      'max-width': '100%'
+    },
+    blockquote: {
+      'margin': '0 0 30px',
+      'padding': '40px 15px 1px',
+      'background-color': '#f8f8f8',
+      'background-image': 'url(../../assets/images/blockquote.svg)',
+      'background-position': 'left 15px top 20px',
+      'background-repeat': 'no-repeat',
+      'color': 'rgba(0,0,0,.68)'
+    },
+    code: {
+      'font-size': '90%',
+      'overflow': 'auto',
+      'color': '#333',
+      'background': 'rgb(247, 247, 247)',
+      'border-radius': '2px',
+      'padding': '10px',
+      'line-height': '1.3',
+      'border': '1px solid rgb(236,236,236)',
+      'margin': '0 0 30px'
+    },
+    image: {
+      'height': 'auto',
+      'border-radius': '5px',
+      'display': 'block',
+      'margin': '0 0 30px',
+      'text-align': 'center',
+      'max-width': '100%',
+    },
+    image_org: {
+      'height': 'auto',
+      'border-radius': '5px',
+      'display': 'block',
+      'margin': '0 0 30px',
+      'text-align': 'center',
+      'max-width': '100%',
+    },
+    ol: {
+      'counter-reset': 'sectioncounter',
+      'padding-left': '15px',
+      'margin': '0 0 30px'
+    },
+    ul: {
+      'counter-reset': 'sectioncounter',
+      'padding-left': '15px',
+      'margin': '0 0 30px'
+    },
+    footnote: {
+      'font-size': '14px',
+      'margin': '20px 0 0',
+      'color': '#f1404b'
+    },
+    footnotes: {
+      'margin': '0',
+      'font-size': '12px'
+    }  
+  },
+  inline: {
+    listitem: {
+      'display': 'block',
+      'position': 'relative',
+      'margin': '0 0 5px',
+      'color': 'rgba(0,0,0,.6)',
+      'font-size': '14px'
+    },
+    codespan: {
+      'font-size': '90%',
+      'color': '#f1404b',
+      'background': '#f8f5ec',
+      'padding': '3px 5px',
+      'border-radius': '4px',
+    },
+    link: {
+      'color': '#f1404b'
+    },
+    strong: {
+      'font-weight': 'bold',
+      'font-size': '1rem',
+    },
+    table: {
+      'border-collapse': 'collapse',
+      'margin': '0 0 30px',
+      'overflow': 'auto',
+      'width': '100%'
+    },
+    thead: {
+      'background': 'rgba(0,0,0,0.05)',
+    },
+    td: {
+      'font-size': '80%',
+      'border': '1px solid #dfdfdf',
+      'padding': '4px 8px',
+    }
+  }
+}
